0c4bcc3a0c075a6afb94599af2d1ba0ed08ac900
RaulHappyCode <raul.faria@happycodeschool.com>
2019-02-14 18:59:11 -0200

    atualizacoes de hoje

--- RaulMaps/src/app/home/home.module.ts
+++ RaulMaps/src/app/home/home.module.ts
@@ -1,23 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import{Component, ViewChild, ElementRef} from '@angular/core';
+import{NavController, Platform} from '@ionic/angular';
 
-import { HomePage } from './home.page';
-
-@NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
-  declarations: [HomePage]
+@Component({
+  selector:'page-home',
+  templateUrl: 'home.page.html'
 })
-export class HomePageModule {}
+export class HomePage {
+  constructor(public navCtrl: NavController){
+
+  }
+}
